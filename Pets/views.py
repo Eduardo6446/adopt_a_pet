b46b15764a750112a7a3212984f5661f790ef268
Eduardo6446 <eduorozco8@gmail.com>
2022-06-21 13:03:14 -0600

    layout hecho

--- Pets/views.py
+++ Pets/views.py
@@ -13,3 +13,13 @@ from .models import *
 # Create your views here.
 def index(request):
     return render(request, 'Pets/index.html')
+
+def login_view(request):
+    return render(request, 'Pets/login.html')
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+def register_view(request):
+    return render(request, 'Pets/register.html')
