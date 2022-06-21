99d41fb072b800b72b6e0244bec0b3db07499308
Eduardo6446 <eduorozco8@gmail.com>
2022-06-21 09:17:04 -0600

    modelos hechos

--- Pets/views.py
+++ Pets/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator
+import json
+
+from .models import *
 
 # Create your views here.
 def index(request):
